976093971aac1c34db4d94ff39b2d678315f3c6b
embetapbay123 <175529093+embetapbay123@users.noreply.github.com>
2026-05-15 17:00:00 +0700

    feat: add cafe domain models and local repository

---
+++ lib/cafes/repositories/cafe_repository.dart
@@ -0,0 +1,25 @@
+import '../models/cafe.dart';
+import '../models/collection.dart';
+import '../models/review.dart';
+import '../models/user_profile.dart';
+
+abstract class CafeRepository {
+  Future<List<Cafe>> getCafes();
+  Future<Cafe?> getCafeById(String id);
+  Future<List<Cafe>> searchCafes({
+    required String query,
+    List<String> filters = const [],
+  });
+  Future<List<Cafe>> getFavouriteCafes();
+  Future<List<Review>> getReviewHistory();
+  Future<List<CafeCollection>> getCollections();
+  Future<UserProfile> getUserProfile();
+  Future<void> toggleFavourite(String cafeId);
+  Future<void> addReview(
+    String cafeId,
+    Review review, {
+    String? imagePath,
+  });
+  Future<void> createCollection(String name, List<String> cafeIds);
+  Future<void> updateUserProfile(UserProfile profile);
+}
